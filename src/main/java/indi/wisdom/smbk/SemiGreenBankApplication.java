5c87a86d2d582ec3f4d0ea6f00d81d7a98ea7d12
wisdom <wisdomking176@163.com>
2024-09-01 14:18:41 +0800

    init

---
+++ src/main/java/indi/wisdom/smbk/SemiGreenBankApplication.java
@@ -0,0 +1,13 @@
+package indi.wisdom.smbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiGreenBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiGreenBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/indi/wisdom/smbk/SemiGreenBankApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.wisdom.smbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiGreenBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
